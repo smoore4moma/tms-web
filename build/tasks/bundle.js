12f1b4e8448b0cb3cd660758afd1fed19047a45e
smoore4moma <steven_moore@moma.org>
2016-05-30 16:00:53 -0400

    Remove gulp watch

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,19 +1,49 @@
+    
 var gulp = require('gulp');
 var bundler = require('aurelia-bundler');
-var bundles = require('../bundles.js');
 
 var config = {
   force: true,
-  baseURL: '.',
-  configPath: './config.js',
-  bundles: bundles.bundles
+  baseURL: '.',                   // baseURL of the application
+  configPath: './config.js',      // config.js file. Must be within `baseURL`
+  bundles: {
+    "dist/app-build": {           // bundle name/path. Must be within `baseURL`. Final path is: `baseURL/dist/app-build.js`.
+      includes: [
+        '[*.js]',
+        '*.html!text',
+        '*.css!text',        
+      ],
+      options: {
+        inject: true,
+        minify: true
+      }
+    },
+    "dist/vendor-build": {
+      includes: [
+      'aurelia-framework',
+      'aurelia-bootstrapper',
+      'aurelia-router',
+      'aurelia-fetch-client',
+      'aurelia-validation',
+      'bootstrap',
+      'font-awesome'
+      ],
+      options: {
+        inject: true,
+        minify: true
+      }
+    }
+  }
 };
 
-gulp.task('bundle', ['unbundle', 'build'], function() {
-  return bundler.bundle(config);
+//gulp.task('bundle', function() {
+//  return bundle(config);
+//});
+
+gulp.task('bundle', function() {  
+ return bundler.bundle(config);
 });
 
-gulp.task('unbundle', function() {
-  return bundler.unbundle(config);
+gulp.task('unbundle', function() {  
+ return bundler.unbundle(config);
 });
-
